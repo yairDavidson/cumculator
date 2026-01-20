46b4736030321869976e4c9bc9f01e647425a046
yaird <yairdavidson10@gmail.com>
2026-01-20 10:49:18 +0200

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,3 +53,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
